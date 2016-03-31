31857de587d854f29553d2ee7f88fadee7d37993
Julio Patrick <sanzher@gmail.com>
2016-03-30 21:57:01 -0300

    Actualización, de direccion de DB

--- matrices/conexionsql.php
+++ matrices/conexionsql.php
@@ -4,6 +4,6 @@
 
 $link = mysql_connect('190.107.177.242', 'cin1770_jonathan', 'realmadrid2013');
         //$link = mysql_connect('localhost', 'root', '');
-mysql_select_db('cin1770_miQA');
+mysql_select_db('cin1770_sca');
 
 ?>
